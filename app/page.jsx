89e44eaa7a504e3cce8ddd17a3b13a797a135c87
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2023-12-18 03:06:40 +0530

    Migration to NextJS from CRA. (#3)
    * Migration to NextJS from CRA.
    
    * Fixes for prod build.
    
    * CRA references removed from Readme.
    
    * Readme updated.

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import HomeLoansEmi from 'pages/HomeLoansEmi'
+
+const Home = () => {
+  // return (
+  //   <div>
+  //     <h1>Home page v2</h1>
+  //   </div>
+  // )
+  return <HomeLoansEmi />
+}
+
+export default Home
