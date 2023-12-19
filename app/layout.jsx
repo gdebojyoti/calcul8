00f90e14b4e5756c336ba256bafb19f1021625da
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2023-12-19 06:37:58 +0530

    Basic navigation.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,15 @@
 const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body>
+        <nav>
+          <ul>
+            <li><a href='/'>Home</a></li>
+            <li><a href='/emi'>Sample page</a></li>
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
