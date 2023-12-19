03b38de00ebe916ab6dfeb437196510485fd4492
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2023-12-19 06:30:51 +0530

    Meta data support. Favicon changed. (#4)
    * Meta data support.
    
    * Favicon changed.

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,16 @@
 import HomeLoansEmi from 'pages/HomeLoansEmi'
 
 const Home = () => {
-  // return (
-  //   <div>
-  //     <h1>Home page v2</h1>
-  //   </div>
-  // )
-  return <HomeLoansEmi />
+  return (
+    <>
+      <HomeLoansEmi />
+    </>
+  )
+}
+
+export const metadata = {
+  title: 'Welcome to Project CALCUL8',
+  description: 'The all-in-one destination for making informed choices and planning your financial future effortlessly. Get accurate insights for smarter money management'
 }
 
 export default Home
